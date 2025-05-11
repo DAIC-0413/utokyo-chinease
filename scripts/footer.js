a1f047744bd2eb4165c5cc5048e8c6588723e1c3
DAIC-0413 <sawachikadaichi@g.ecc.u-tokyo.ac.jp>
2025-05-11 10:32:10 +0900

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -4,6 +4,7 @@ const footer = document.createElement("footer");
 footer.innerHTML = `
   © Sawachika — このサイトは東京大学の学生向けに学習支援を目的として制作されました。<br>
   本サイトの内容・構造はすべて非営利・教育目的で提供されています。東京大学とは何ら関係ありません<br>
+  一緒に作成したい人はこちらまで：sawacikadaichi@outlook.jp
   ver1.0
 `;
 document.body.appendChild(footer);
